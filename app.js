e99368502a8b8c07615379b2dfc64da216150bda
Wesley Carlos <w.esleycarlos@hotmail.com>
2017-08-13 06:24:43 -0300

    Importação de modulos e auto execução dos mesmos.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const portApp = 4000;
 
+const serverConfig = require("./config/server.config")();
+
+const indexRoute = require("./routes/index.route")();
+const livrosRoute = require("./routes/livros.route")();
 
 app.listen(portApp, function(){
     console.log(`Servidor rodando na porta ${portApp}`);
