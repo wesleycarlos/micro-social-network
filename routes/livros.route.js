b79b29d18885a2548c8c6669823c331be1c3ab80
Wesley Carlos <w.esleycarlos@hotmail.com>
2017-08-13 06:20:31 -0300

    Criação de recurso de exclusão, criação e visualização de livros.

--- routes/livros.route.js
+++ routes/livros.route.js
@@ -1,5 +1,44 @@
-module.exports = function(app){
-    app.get("/livros", function(req, res){
-        res.send("Livros");
+module.exports = function (app) {
+    const livroModel = require("../models/livro.model");
+
+
+    app.get("/livros", function (req, res) {
+        livroModel.find(function (error, response) {
+            if (error) {
+                res.json(error);
+            }
+            res.json(response);
+        });
     });
+
+    app.post("/cadastrar-livro", function (req, res) {
+        const livroModelo = {
+            titulo: req.body.titulo,
+            autor: req.body.autor,
+            numPaginas: req.body.numPaginas,
+            anoLanca: req.body.anoLanca
+        }
+
+        const livroExec = new livroModel(livroModelo);
+        livroExec.save(function (error, response) {
+            if (error) {
+                res.json(error);
+            }
+            res.json(response);
+        });
+
+
+    });
+
+    app.delete("/delete-livro/:autor", function(req, res){
+        livroModel.deleteMany({autor: req.params.autor}, function(error, response){
+            if (error){
+                res.json(error);
+            }
+            res.json(response);
+        })
+    })
 };
+
+
+
