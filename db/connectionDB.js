d1a7bfc80dd8bf709e60581f4da7c335bb71e591
Wesley Carlos <w.esleycarlos@hotmail.com>
2017-08-14 23:08:06 -0300

    Adição do link de conexão com DB externo

--- db/connectionDB.js
+++ db/connectionDB.js
@@ -1,4 +1,4 @@
 module.exports = function(){
-    const mongoose = require('mongoose');
+    const  mongoose = require('mongoose');
     mongoose.connect('mongodb://wesley:123456@ds119091.mlab.com:19091/cadastro-livros');
 };
