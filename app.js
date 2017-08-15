73893d2a3239fa73f92c8651ba0ae284523e2cbd
Wesley Carlos <w.esleycarlos@hotmail.com>
2017-08-14 21:58:01 -0300

    Solução de erros.

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const portApp = 4000;
 
-const serverConfig = require("./config/server.config")();
+const serverConfig = require("./config/server.config")(app);
 
-const indexRoute = require("./routes/index.route")();
-const livrosRoute = require("./routes/livros.route")();
+const indexRoute = require("./routes/index.route")(app);
+const livrosRoute = require("./routes/livros.route")(app);
 
 app.listen(portApp, function(){
     console.log(`Servidor rodando na porta ${portApp}`);
